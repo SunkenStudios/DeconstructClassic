0b97ce0a54318be54b11e2b0fce859bf6614cdb9
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2025-03-17 15:29:41 -0700

    GUI Setup Stuff

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace DeconstructClassic
 {
@@ -7,6 +10,32 @@ namespace DeconstructClassic
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Menu_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (sender is Border)
+                BeginMoveDrag(e);
+        }
+
+        private void MenuItem_Click(object? sender, RoutedEventArgs e)
+        {
+            MenuItem item = (MenuItem)sender!;
+            switch (item.Name)
+            {
+                case "OpenFile":
+                    var file = TopLevel.GetTopLevel(this).StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                    {
+                        Title = "Open File",
+                        AllowMultiple = false
+                    }).Result;
+
+                    if (file.Count >= 1)
+                    {
+                        //FileTree.LoadFromFile(file[0].Path.LocalPath);
+                    }
+                    break;
+            }
         }
     }
 }
