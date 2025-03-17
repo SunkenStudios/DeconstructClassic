dbb60eec5c3ed2acd5b0251216ee196fb6709a19
SunkenStudios <90426101+SunkenStudios@users.noreply.github.com>
2025-03-17 17:13:09 -0500

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DeconstructClassic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
