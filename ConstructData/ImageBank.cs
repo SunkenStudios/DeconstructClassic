914110852586044a8ca14b3c519b063c84e8f6f3
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2025-03-17 20:00:12 -0700

    Added ImageBank reading

---
+++ ConstructData/ImageBank.cs
@@ -0,0 +1,16 @@
+﻿using DeconstructClassic.Memory;
+
+namespace DeconstructClassic.ConstructData
+{
+    public class ImageBank
+    {
+        public ImageEntry[] Images;
+
+        public ImageBank(ByteReader reader)
+        {
+            Images = new ImageEntry[reader.ReadInt()];
+            for (int i = 0; i < Images.Length; i++)
+                Images[i] = new ImageEntry(reader);
+        }
+    }
+}
---
+++ ConstructData/ImageEntry.cs
@@ -0,0 +1,33 @@
+﻿using DeconstructClassic.Memory;
+using System.Numerics;
+
+namespace DeconstructClassic.ConstructData
+{
+    public class ImageEntry
+    {
+        public int ID;
+        public Vector2 Hotspot;
+        public Vector2[] ActionPoints;
+        public byte[] Data;
+
+        public int CollisionMaskWidth;
+        public int CollisionMaskHeight;
+        public int CollisionMaskPitch;
+        public byte[] CollisionMaskData;
+
+        public ImageEntry(ByteReader reader)
+        {
+            ID = reader.ReadInt();
+            Hotspot = new Vector2(reader.ReadInt(), reader.ReadInt());
+            ActionPoints = new Vector2[reader.ReadInt()];
+            for (int i = 0; i < ActionPoints.Length; i++)
+                ActionPoints[i] = new Vector2(reader.ReadInt(), reader.ReadInt());
+            Data = reader.ReadBytes(reader.ReadInt());
+
+            CollisionMaskWidth = reader.ReadInt();
+            CollisionMaskHeight = reader.ReadInt();
+            CollisionMaskPitch = reader.ReadInt();
+            CollisionMaskData = reader.ReadBytes(CollisionMaskPitch * CollisionMaskHeight);
+        }
+    }
+}
